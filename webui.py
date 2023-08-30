7fdf21b08c8416e47a693f67bb680faea27b6756
Runew0lf <runew0lf@gmail.com>
2023-08-30 22:14:16 +0100

    fixed listen wasnt responding to remote access

--- webui.py
+++ webui.py
@@ -246,7 +246,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--port", type=int, default=None, help="Set the listen port.")
 parser.add_argument("--share", action="store_true", help="Set whether to share on Gradio.")
 parser.add_argument(
-    "--listen", type=str, default=None, metavar="IP", nargs="?", const="127.0.0.1", help="Set the listen interface."
+    "--listen", type=str, default=None, metavar="IP", nargs="?", const="0.0.0.0", help="Set the listen interface."
 )
 parser.add_argument("--nobrowser", action="store_true", help="Do not launch in browser.")
 args = parser.parse_args()
