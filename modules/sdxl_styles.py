554a52697cad6065ca4cb61b4ee9ce17ede412f8
Runew0lf <runew0lf@gmail.com>
2023-08-30 10:08:08 +0100

    refactored and tidied code

--- modules/sdxl_styles.py
+++ modules/sdxl_styles.py
@@ -1,55 +1,64 @@
-# https://github.com/twri/sdxl_prompt_styler/blob/main/sdxl_styles.json
 import os
 import shutil
 from csv import DictReader, reader
 
-styles = []
-
-# Check if styles.csv file exists
-if not os.path.isfile("styles.csv"):
-    shutil.copy("styles.default", "styles.csv")
-
-with open("styles.csv", "r") as file:
-    # Create DictReader object to read CSV as dictionaries
-    treader = DictReader(file)
-    # Create default styles list
-    default_styles = [
-        {"name": "None", "prompt": "{prompt}", "negative_prompt": ""},
-    ]
-    # Read in styles from CSV into styles list
-    styles = list(treader)
-    # Insert default styles at start of styles list
-    for item in default_styles:
-        styles.insert(0, item)
+DEFAULT_STYLES_FILE = "styles.default"
+STYLES_FILE = "styles.csv"
+DEFAULT_RESOLUTIONS_FILE = "resolutions.default"
+RESOLUTIONS_FILE = "resolutions.csv"
 
 
-styles = {k["name"]: (k["prompt"], k["negative_prompt"]) for k in styles}
+def load_styles():
+    styles = []
+
+    if not os.path.isfile(STYLES_FILE):
+        shutil.copy(DEFAULT_STYLES_FILE, STYLES_FILE)
+
+    with open(STYLES_FILE, "r") as f:
+        reader = DictReader(f)
+        styles = list(reader)
+
+    default_style = {"name": "None", "prompt": "{prompt}", "negative_prompt": ""}
+    styles.insert(0, default_style)
+
+    return {s["name"]: (s["prompt"], s["negative_prompt"]) for s in styles}
+
+
+def load_resolutions():
+    ratios = {}
+
+    if not os.path.isfile(RESOLUTIONS_FILE):
+        shutil.copy(DEFAULT_RESOLUTIONS_FILE, RESOLUTIONS_FILE)
+
+    with open(RESOLUTIONS_FILE, "r") as f:
+        reader = DictReader(f)
+        for row in reader:
+            ratios[row["ratio"]] = (int(row["width"]), int(row["height"]))
+
+    return ratios
+
+
+def apply_style(style, prompt, negative_prompt):
+    output_prompt = ""
+    output_negative_prompt = ""
+
+    if not style:
+        return prompt, negative_prompt
+
+    for s in style:
+        p, n = styles.get(s, default_style)
+        output_prompt += p + ", "
+        output_negative_prompt += n + ", "
+
+    output_prompt = output_prompt.replace("{prompt}", prompt)
+    output_negative_prompt += ", " + negative_prompt
+
+    return output_prompt, output_negative_prompt
+
+
+styles = load_styles()
 default_style = styles["None"]
 style_keys = list(styles.keys())
 
-SD_XL_BASE_RATIOS = {}
-
-# Check if resolutions.csv file exists
-if not os.path.isfile("resolutions.csv"):
-    shutil.copy("resolutions.default", "resolutions.csv")
-
-with open("resolutions.csv", "r") as file:
-    reader = DictReader(file)
-    for row in reader:
-        SD_XL_BASE_RATIOS[row["ratio"]] = (int(row["width"]), int(row["height"]))
-
-
+SD_XL_BASE_RATIOS = load_resolutions()
 aspect_ratios = {f"{v[0]}x{v[1]} ({k})": v for k, v in SD_XL_BASE_RATIOS.items()}
-
-
-def apply_style(style, positive, negative):
-    prompt = ""
-    negative_prompt = ""
-    if not style:
-        return positive, negative
-    for item in style:
-        p, n = styles.get(item, default_style)
-        prompt += p + ", "
-        negative_prompt += n + ", "
-
-    return prompt.replace("{prompt}", positive), negative_prompt + ", " + negative
