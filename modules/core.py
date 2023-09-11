f18eb1ecdffc1b3e952cfa73f5768d8d903d5267
Jonas Stare <yownas@gmail.com>
2023-09-11 23:56:53 +0200

    Cleanup

--- modules/core.py
+++ modules/core.py
@@ -105,117 +105,6 @@ def get_previewer(device, latent_format):
 
     return taesd
 
-
-@torch.no_grad()
-def ksampler(
-    model,
-    positive,
-    negative,
-    latent,
-    seed=None,
-    steps=30,
-    cfg=7.0,
-    sampler_name="dpmpp_2m_sde_gpu",
-    scheduler="karras",
-    denoise=1.0,
-    disable_noise=False,
-    start_step=None,
-    last_step=None,
-    force_full_denoise=False,
-    callback_function=None,
-):
-    # SCHEDULERS = ["normal", "karras", "exponential", "simple", "ddim_uniform"]
-    # SAMPLERS = ["euler", "euler_ancestral", "heun", "dpm_2", "dpm_2_ancestral",
-    #             "lms", "dpm_fast", "dpm_adaptive", "dpmpp_2s_ancestral", "dpmpp_sde", "dpmpp_sde_gpu",
-    #             "dpmpp_2m", "dpmpp_2m_sde", "dpmpp_2m_sde_gpu", "ddim", "uni_pc", "uni_pc_bh2"]
-
-    seed = seed if isinstance(seed, int) else random.randint(1, 2**64)
-
-    device = comfy.model_management.get_torch_device()
-    latent_image = latent["samples"]
-
-    if disable_noise:
-        noise = torch.zeros(latent_image.size(), dtype=latent_image.dtype, layout=latent_image.layout, device="cpu")
-    else:
-        batch_inds = latent["batch_index"] if "batch_index" in latent else None
-        noise = comfy.sample.prepare_noise(latent_image, seed, batch_inds)
-
-    noise_mask = None
-    if "noise_mask" in latent:
-        noise_mask = latent["noise_mask"]
-
-    previewer = get_previewer(device, model.model.latent_format)
-
-    pbar = comfy.utils.ProgressBar(steps)
-
-    def callback(step, x0, x, total_steps):
-        y = None
-        if previewer:
-            y = previewer.preview(x0, step, total_steps)
-        if callback_function is not None:
-            callback_function(step, x0, x, total_steps, y)
-        pbar.update_absolute(step + 1, total_steps, None)
-
-    sigmas = None
-    disable_pbar = False
-
-    if noise_mask is not None:
-        noise_mask = prepare_mask(noise_mask, noise.shape, device)
-
-    real_model = None
-    models, inference_memory = get_additional_models(positive, negative, model.model_dtype())
-    with suppress_stdout():
-        comfy.model_management.load_models_gpu(
-            [model] + models,
-            comfy.model_management.batch_area_memory(noise.shape[0] * noise.shape[2] * noise.shape[3])
-            + inference_memory,
-        )
-    real_model = model.model
-
-    noise = noise.to(device)
-    latent_image = latent_image.to(device)
-
-    positive_copy = broadcast_cond(positive, noise.shape[0], device)
-    negative_copy = broadcast_cond(negative, noise.shape[0], device)
-
-    models = get_additional_models(positive, negative, model.model_dtype())
-
-    sampler = KSampler(
-        real_model,
-        steps=steps,
-        device=device,
-        sampler=sampler_name,
-        scheduler=scheduler,
-        denoise=denoise,
-        model_options=model.model_options,
-    )
-
-    samples = sampler.sample(
-        noise,
-        positive_copy,
-        negative_copy,
-        cfg=cfg,
-        latent_image=latent_image,
-        start_step=start_step,
-        last_step=last_step,
-        force_full_denoise=force_full_denoise,
-        denoise_mask=noise_mask,
-        sigmas=sigmas,
-        callback=callback,
-        disable_pbar=disable_pbar,
-        seed=seed,
-    )
-
-    samples = samples.cpu()
-
-    cleanup_additional_models(models)
-
-    out = latent.copy()
-    out["samples"] = samples
-
-    return out
-
-
 @torch.no_grad()
 def ksampler_with_refiner(
     model,
@@ -283,6 +172,7 @@ def ksampler_with_refiner(
             comfy.model_management.batch_area_memory(noise.shape[0] * noise.shape[2] * noise.shape[3])
             + inference_memory,
         )
+    real_model = model.model
 
     noise = noise.to(device)
     latent_image = latent_image.to(device)
@@ -290,37 +180,54 @@ def ksampler_with_refiner(
     positive_copy = broadcast_cond(positive, noise.shape[0], device)
     negative_copy = broadcast_cond(negative, noise.shape[0], device)
 
-    refiner_positive_copy = broadcast_cond(refiner_positive, noise.shape[0], device)
-    refiner_negative_copy = broadcast_cond(refiner_negative, noise.shape[0], device)
+    kwargs = {
+        "cfg": cfg,
+        "latent_image": latent_image,
+        "start_step": start_step,
+        "last_step": last_step,
+        "force_full_denoise": force_full_denoise,
+        "denoise_mask": noise_mask,
+        "sigmas": sigmas,
+        "callback": callback,
+        "disable_pbar": disable_pbar,
+        "seed": seed,
+    }
+    if refiner is not None:
+        refiner_positive_copy = broadcast_cond(refiner_positive, noise.shape[0], device)
+        refiner_negative_copy = broadcast_cond(refiner_negative, noise.shape[0], device)
 
-    sampler = KSamplerWithRefiner(
-        model=model,
-        refiner_model=refiner,
-        steps=steps,
-        device=device,
-        sampler=sampler_name,
-        scheduler=scheduler,
-        denoise=denoise,
-        model_options=model.model_options,
-    )
+        sampler = KSamplerWithRefiner(
+            model=model,
+            refiner_model=refiner,
+            steps=steps,
+            device=device,
+            sampler=sampler_name,
+            scheduler=scheduler,
+            denoise=denoise,
+            model_options=model.model_options,
+        )
+        extra_kwargs = {
+            "refiner_positive": refiner_positive_copy,
+            "refiner_negative": refiner_negative_copy,
+            "refiner_switch_step": refiner_switch_step,
+        }
+        kwargs.update(extra_kwargs)
+    else:
+        sampler = KSampler(
+            real_model,
+            steps=steps,
+            device=device,
+            sampler=sampler_name,
+            scheduler=scheduler,
+            denoise=denoise,
+            model_options=model.model_options,
+        )
 
     samples = sampler.sample(
         noise,
         positive_copy,
         negative_copy,
-        refiner_positive=refiner_positive_copy,
-        refiner_negative=refiner_negative_copy,
-        refiner_switch_step=refiner_switch_step,
-        cfg=cfg,
-        latent_image=latent_image,
-        start_step=start_step,
-        last_step=last_step,
-        force_full_denoise=force_full_denoise,
-        denoise_mask=noise_mask,
-        sigmas=sigmas,
-        callback_function=callback,
-        disable_pbar=disable_pbar,
-        seed=seed,
+        **kwargs
     )
 
     samples = samples.cpu()
