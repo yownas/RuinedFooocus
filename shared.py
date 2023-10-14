f1f802116ff23d2d159bea11aee3faba6429b792
Jonas Stare <yownas@gmail.com>
2023-10-14 19:36:59 +0200

    fix

--- shared.py
+++ shared.py
@@ -1,6 +1,11 @@
 gradio_root = None
 
-state = {"preview_image": None, "ctrls_name": [], "ctrls_obj": []}
+state = {
+    "preview_grid": None,
+    "preview_image": None,
+    "ctrls_name": [],
+    "ctrls_obj": []
+}
 
 
 def add_ctrl(name, obj):
