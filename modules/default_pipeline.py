ad53394a472774bbda8a9757ca03e8b8cc797103
Jonas Stare <yownas@gmail.com>
2023-10-30 21:10:21 +0100

    No more core

--- modules/default_pipeline.py
+++ modules/default_pipeline.py
@@ -4,7 +4,6 @@ import os
 import torch
 import warnings
 
-import modules.core as core
 import modules.path
 import modules.controlnet
 import modules.async_worker as worker
@@ -18,7 +17,9 @@ from modules.util import suppress_stdout
 import warnings
 import time
 
+import einops
 import comfy.utils
+import comfy.model_management
 from comfy.sd import load_checkpoint_guess_config
 from nodes import (
     CLIPTextEncode,
@@ -27,9 +28,17 @@ from nodes import (
     VAEDecode,
     VAEEncode,
 )
+from comfy.sample import (
+    cleanup_additional_models,
+    convert_cond,
+    get_additional_models,
+    prepare_mask,
+)
+from comfy.samplers import KSampler
 from comfy_extras.nodes_post_processing import ImageScaleToTotalPixels
 from comfy_extras.nodes_canny import Canny
 
+comfy.model_management.DISABLE_SMART_MEMORY = True
 
 
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -48,6 +57,40 @@ class StableDiffusionModel:
             self.clip.cond_stage_model.to("meta")
         if self.vae is not None:
             self.vae.first_stage_model.to("meta")
+
+def get_previewer(device, latent_format):
+    from latent_preview import TAESD, TAESDPreviewerImpl
+
+    taesd_decoder_path = os.path.abspath(
+        os.path.realpath(
+            os.path.join("models", "vae_approx", latent_format.taesd_decoder_name)
+        )
+    )
+
+    if not os.path.exists(taesd_decoder_path):
+        print(
+            f"Warning: TAESD previews enabled, but could not find {taesd_decoder_path}"
+        )
+        return None
+
+    taesd = TAESD(None, taesd_decoder_path).to(device)
+
+    def preview_function(x0, step, total_steps):
+        global cv2_is_top
+        with torch.no_grad():
+            x_sample = (
+                taesd.decoder(
+                    torch.nn.functional.avg_pool2d(x0, kernel_size=(2, 2))
+                ).detach()
+                * 255.0
+            )
+            x_sample = einops.rearrange(x_sample, "b c h w -> b h w c")
+            x_sample = x_sample.cpu().numpy().clip(0, 255).astype(np.uint8)
+            return x_sample[0]
+
+    taesd.preview = preview_function
+
+    return taesd
 
 
 xl_base: StableDiffusionModel = None
@@ -251,23 +294,95 @@ def process(
 
     worker.outputs.append(["preview", (-1, f"Start sampling ...", None)])
 
-    sampled_latent = core.ksampler(
-        model=xl_base_patched.unet,
-        positive=positive_conditions_cache,
-        negative=negative_conditions_cache,
-        latent=latent,
-        steps=steps,
-        start_step=start_step,
-        last_step=steps,
-        disable_noise=False,
-        force_full_denoise=force_full_denoise,
-        denoise=denoise,
-        seed=image_seed,
-        sampler_name=sampler_name,
-        scheduler=scheduler,
-        cfg=cfg,
-        callback_function=callback,
+    seed = image_seed if isinstance(image_seed, int) else random.randint(1, 2**64)
+
+    device = comfy.model_management.get_torch_device()
+    latent_image = latent["samples"]
+    #if disable_noise:
+    #    noise = torch.zeros(
+    #        latent_image.size(),
+    #        dtype=latent_image.dtype,
+    #        layout=latent_image.layout,
+    #        device="cpu",
+    #    )
+    #else:
+    if True:
+        batch_inds = latent["batch_index"] if "batch_index" in latent else None
+        noise = comfy.sample.prepare_noise(latent_image, seed, batch_inds)
+
+    noise_mask = None
+    if "noise_mask" in latent:
+        noise_mask = latent["noise_mask"]
+
+    previewer = get_previewer(device, xl_base_patched.unet.model.latent_format)
+
+    pbar = comfy.utils.ProgressBar(steps)
+
+    def callback_function(step, x0, x, total_steps):
+        y = None
+        if previewer:
+            y = previewer.preview(x0, step, total_steps)
+        if callback is not None:
+            callback(step, x0, x, total_steps, y)
+        pbar.update_absolute(step + 1, total_steps, None)
+
+    sigmas = None
+    disable_pbar = False
+
+    if noise_mask is not None:
+        noise_mask = prepare_mask(noise_mask, noise.shape, device)
+
+    models, inference_memory = get_additional_models(
+        positive_conditions_cache, negative_conditions_cache, xl_base_patched.unet.model_dtype()
     )
+    with suppress_stdout():
+        comfy.model_management.load_models_gpu(
+            [xl_base_patched.unet] + models,
+            comfy.model_management.batch_area_memory(
+                noise.shape[0] * noise.shape[2] * noise.shape[3]
+            )
+            + inference_memory,
+        )
+    real_model = xl_base_patched.unet.model
+
+    noise = noise.to(device)
+    latent_image = latent_image.to(device)
+
+    positive_copy = convert_cond(positive_conditions_cache)
+    negative_copy = convert_cond(negative_conditions_cache)
+    kwargs = {
+        "cfg": cfg,
+        "latent_image": latent_image,
+        "start_step": start_step,
+        "last_step": steps,
+        "force_full_denoise": force_full_denoise,
+        "denoise_mask": noise_mask,
+        "sigmas": sigmas,
+        "disable_pbar": disable_pbar,
+        "seed": seed,
+    }
+    sampler = KSampler(
+        real_model,
+        steps=steps,
+        device=device,
+        sampler=sampler_name,
+        scheduler=scheduler,
+        denoise=denoise,
+        model_options=xl_base_patched.unet.model_options,
+    )
+    extra_kwargs = {
+        "callback": callback_function,
+    }
+    kwargs.update(extra_kwargs)
+
+    samples = sampler.sample(noise, positive_copy, negative_copy, **kwargs)
+
+    samples = samples.cpu()
+
+    cleanup_additional_models(models)
+
+    sampled_latent = latent.copy()
+    sampled_latent["samples"] = samples
 
     worker.outputs.append(["preview", (-1, f"VAE decoding ...", None)])
 
