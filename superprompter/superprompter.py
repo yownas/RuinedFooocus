b44c45bb46891f15d3e37ae3eaa6eea57bdf9494
Runew0lf <runew0lf@gmail.com>
2024-04-17 09:27:11 +0100

    fixed hyperprompt model files to use pathlib

--- superprompter/superprompter.py
+++ superprompter/superprompter.py
@@ -4,27 +4,25 @@ import random
 import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from superprompter.download_models import download_models
+from pathlib import Path
 
 global tokenizer, model
-script_dir = os.path.dirname(os.path.abspath(__file__))  # Script directory
-modelDir = os.path.join(script_dir, "./model_files/" )
+script_dir = Path(__file__).resolve().parent  # Script directory
+modelDir = script_dir / "model_files"
+
 
 def load_models():
- 
-    if not all(os.path.exists(modelDir) for file in modelDir):
+
+    if not os.listdir(modelDir):
         print("Model files not found. Downloading...\n")
-        download_models()
-    # else:
-        # print("Model files found. Skipping download.\n")
-
-    # print("Loading SuperPrompt-v1 model...\n")
-
+        download_models(modelDir)
 
     global tokenizer, model
     tokenizer = T5Tokenizer.from_pretrained(modelDir)
-    model = T5ForConditionalGeneration.from_pretrained(modelDir, torch_dtype=torch.float16)
+    model = T5ForConditionalGeneration.from_pretrained(
+        modelDir, torch_dtype=torch.float16
+    )
 
-    # print("SuperPrompt-v1 model loaded successfully.\n")
 
 def unload_models():
     global tokenizer, model
@@ -36,25 +34,39 @@ def unload_models():
     os.rmdir(modelDir)
 
 
+def answer(
+    input_text="",
+    max_new_tokens=512,
+    repetition_penalty=1.2,
+    temperature=0.5,
+    top_p=1,
+    top_k=1,
+    seed=-1,
+):
 
-def answer(input_text="", max_new_tokens=512, repetition_penalty=1.2, temperature=0.5, top_p=1, top_k = 1 , seed=-1):
-       
     if seed == -1:
         seed = random.randint(1, 1000000)
 
     torch.manual_seed(seed)
 
     if torch.cuda.is_available():
-        device = 'cuda'
+        device = "cuda"
     else:
-        device = 'cpu'
+        device = "cpu"
 
     input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to(device)
     if torch.cuda.is_available():
-        model.to('cuda')
+        model.to("cuda")
 
-    outputs = model.generate(input_ids, max_new_tokens=max_new_tokens, repetition_penalty=repetition_penalty,
-                            do_sample=True, temperature=temperature, top_p=top_p, top_k=top_k)
+    outputs = model.generate(
+        input_ids,
+        max_new_tokens=max_new_tokens,
+        repetition_penalty=repetition_penalty,
+        do_sample=True,
+        temperature=temperature,
+        top_p=top_p,
+        top_k=top_k,
+    )
 
     dirty_text = tokenizer.decode(outputs[0])
     text = dirty_text.replace("<pad>", "").replace("</s>", "").strip()
