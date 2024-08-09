cf7af77cd18e86116d56c46a49a8bd458d47d3e6
Jonas Stare <yownas@gmail.com>
2024-08-09 22:08:57 +0200

    PixArtSigma

--- modules/diffusers_pipeline.py
+++ modules/diffusers_pipeline.py
@@ -5,15 +5,17 @@ import torch
 import diffusers
 import re
 import os
+from pathlib import Path
 
 from PIL import Image
 from shared import path_manager
-
+import json
 
 class pipeline:
     pipeline_type = ["diffusers"]
 
     model_hash = ""
+    model_class = ""
     pipe = None
 
 #    def parse_gen_data(self, gen_data):
@@ -26,11 +28,36 @@ class pipeline:
         print(f"Loading model: {name}")
         self.model_hash = name
         repo_id = re.sub(r"🤗:", "", name, count=1)
+        folder = os.path.join(path_manager.model_paths["diffusers_path"], repo_id)
 
-        # TODO get proper pipeline from model_config.json
-        self.pipe = diffusers.FluxPipeline.from_pretrained(
-            os.path.join(path_manager.model_paths["diffusers_path"], repo_id),
+        model_index = os.path.join(folder, "model_index.json")
+
+        if Path(model_index).exists():
+            try:
+                with open(model_index) as f:
+                    model_json = json.load(f)
+            except:
+                model_json = None
+
+        self.model_class = model_json["_class_name"]
+
+        diffusers_pipeline = None
+        if self.model_class == "PixArtSigmaPipeline":
+            diffusers_pipeline = diffusers.PixArtSigmaPipeline
+        elif self.model_class == "FluxPipeline":
+            diffusers_pipeline = diffusers.FluxPipeline
+#        elif self.model_class == "WuerstchenDecoderPipeline":
+#            diffusers_pipeline = diffusers.AutoPipelineForText2Image
+
+        if diffusers_pipeline == None:
+            print(f"ERRROR: Unknown diffuser pipeline: {model_json['_class_name']}")
+            self.pipe = None
+            return
+
+        self.pipe = diffusers_pipeline.from_pretrained(
+            folder,
             local_files_only = True,
+            cache_dir = os.path.join(path_manager.model_paths["diffusers_path"]),
             torch_dtype=torch.bfloat16
             )
         self.pipe.enable_sequential_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
@@ -79,12 +106,12 @@ class pipeline:
         seed = image_seed
 
         image = self.pipe(
-            positive_prompt,
+            prompt=positive_prompt,
             height=height,
             width=width,
-            guidance_scale=0.0,
+            guidance_scale=cfg,
             output_type="pil",
-            num_inference_steps=4,
+            num_inference_steps=steps,
             max_sequence_length=256,
             generator=torch.Generator("cpu").manual_seed(seed)
         ).images[0]
