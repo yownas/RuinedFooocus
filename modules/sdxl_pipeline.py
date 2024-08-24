d03c08f8bf2b84c359eb4e0cd541c09158ee8252
Jonas Stare <yownas@gmail.com>
2024-08-24 12:56:57 +0200

    Nope. Revert.

--- modules/sdxl_pipeline.py
+++ modules/sdxl_pipeline.py
@@ -64,7 +64,7 @@ from modules.pipleline_utils import (
 class pipeline:
     pipeline_type = ["sdxl", "ssd", "sd3", "flux"]
 
-    comfy.model_management.DISABLE_SMART_MEMORY = True
+    comfy.model_management.DISABLE_SMART_MEMORY = False
 
     class StableDiffusionModel:
         def __init__(self, unet, vae, clip, clip_vision):
@@ -206,12 +206,13 @@ class pipeline:
             with torch.torch.inference_mode():
                 unet, clip, vae, clip_vision = load_checkpoint_guess_config(filename)
 
-            vram_limit = 8*1024*1024*1024
-            vram = comfy.model_management.get_total_memory()
-            model_size = unet.model_size()
-            if vram < vram_limit and model_size >= vram_limit:
-                unet.model.to(dtype=torch.float8_e4m3fn)
-                print(f"Cast unet to float8_e4m3fn")
+#            vram_limit = 11*1024*1024*1024
+#            vram = comfy.model_management.get_total_memory()
+#            model_size = unet.model_size()
+#            if vram < vram_limit and model_size >= vram_limit:
+#            if True:
+#                unet.model.to(dtype=torch.float8_e4m3fn)
+#                print(f"Cast unet to float8_e4m3fn")
 
             self.xl_base = self.StableDiffusionModel(
                 unet=unet, clip=clip, vae=vae, clip_vision=clip_vision
