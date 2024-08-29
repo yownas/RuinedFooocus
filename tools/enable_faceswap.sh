e5e0bf2505e4b9c88232e91e315efb7eae0142f5
Jonas Stare <yownas@gmail.com>
2024-08-29 20:45:32 +0200

    Less modules

--- tools/enable_faceswap.sh
+++ tools/enable_faceswap.sh
@@ -2,7 +2,7 @@
 
 . $(dirname "$(readlink -f "$0")")/../venv/bin/activate
 
-pip install insightface==0.7.3 modelscope==1.17.1 addict==2.4.0 datasets==2.21.0 oss2==2.18.6 simplejson==3.19.3 sortedcontainers==2.4.0 gfpgan==1.3.8 --require-virtualenv
+pip install insightface==0.7.3 gfpgan==1.3.8 --require-virtualenv
 
 cat <<EOF
 pip done...
