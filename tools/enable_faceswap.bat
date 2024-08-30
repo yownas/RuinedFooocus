882aa831e62198e5294c3d6bf151a43766b93304
Jonas Stare <yownas@gmail.com>
2024-08-30 17:39:30 +0200

    Updated scripts

--- tools/enable_faceswap.bat
+++ tools/enable_faceswap.bat
@@ -1,2 +1,21 @@
+@echo off
+echo Some systems fail to install the insightface module, which is why this is not
+echo part of the default install.
+pause
+echo Please wait...
 ..\..\python_embeded\python.exe -m pip install insightface==0.7.3 gfpgan==1.3.8
+echo.
+echo Now add this to your RuinedFooocus\settings\powerup.json
+echo.
+echo "Faceswap": {
+echo    "type": "faceswap"
+echo }
+echo.
+echo Download the required models to models\faceswap\:
+echo.
+echo https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth
+echo and inswapper_128.onnx from where you can find it
+echo.
+echo You can find more info on the RuinedFooocus Github or Discord server.
+echo.
 pause
--- tools/enable_faceswap.sh
+++ tools/enable_faceswap.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+cat <<EOF
+Some systems fail to install the insightface module, which is why this is not
+part of the default install.
+
+Press enter to continue...
+EOF
+read junk
+
 . $(dirname "$(readlink -f "$0")")/../venv/bin/activate
 
 pip install insightface==0.7.3 gfpgan==1.3.8 --require-virtualenv
@@ -14,7 +22,10 @@ Now add this to settings/powerup.json
     "type": "faceswap"
   }
 
-Download models to models/faceswap/
+Download the required models to models\faceswap\:
+
 https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth
-...and inswapper_128.onnx from where you can find it
+and inswapper_128.onnx from where you can find it
+
+You can find more info on the RuinedFooocus Github or Discord server.
 EOF
