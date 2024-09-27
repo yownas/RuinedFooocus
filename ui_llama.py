21dd1383960357a58ace1c4a0437d5579fbbb7f1
Jonas Stare <yownas@gmail.com>
2024-09-27 22:12:24 +0200

    Prettier output

--- ui_llama.py
+++ ui_llama.py
@@ -1,10 +1,13 @@
 import gradio as gr
 import re
 from llama_cpp import Llama
+from modules.util import TimeIt
 
 def add_llama_tab(prompt):
     def run_llama_run(prompt):
-        llm = Llama.from_pretrained(repo_id="hugging-quants/Llama-3.2-3B-Instruct-Q8_0-GGUF", filename="*q8_0.gguf")
+        repo = "hugging-quants/Llama-3.2-3B-Instruct-Q8_0-GGUF"
+        print(f"Loading {repo}")
+        llm = Llama.from_pretrained(repo_id=repo , filename="*q8_0.gguf", verbose=False)
 
         system_prompt = "You are an artist, describe a beautiful scene using the words from the user."
 
@@ -14,15 +17,17 @@ def add_llama_tab(prompt):
             system_prompt = re.sub("system: *", "", system.group(0), flags=re.M|re.I)
             prompt = re.sub(sys_pat, "", prompt)
 
-        try:
-            res = llm.create_chat_completion(
-                messages=[
-                    {"role": "system", "content": system_prompt},
-                    {"role": "user", "content": prompt}
-                ])["choices"][0]["message"]["content"]
-        except Exception as e:
-            print(f"LLAMA ERROR: {e}")
-            res = prompt
+        print("Thinking...")
+        with TimeIt(""):
+            try:
+                res = llm.create_chat_completion(
+                    messages=[
+                        {"role": "system", "content": system_prompt},
+                        {"role": "user", "content": prompt}
+                    ])["choices"][0]["message"]["content"]
+            except Exception as e:
+                print(f"LLAMA ERROR: {e}")
+                res = prompt
 
         llm._stack.close()
         llm.close()
