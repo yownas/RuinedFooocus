a46baa62ca1141a1f592ecd70c956a9a5943369d
Jonas Stare <yownas@gmail.com>
2024-09-27 19:52:07 +0200

    Allow for a system-prompt

--- ui_llama.py
+++ ui_llama.py
@@ -1,4 +1,5 @@
 import gradio as gr
+import re
 from llama_cpp import Llama
 
 def add_llama_tab(prompt):
@@ -6,6 +7,12 @@ def add_llama_tab(prompt):
         llm = Llama.from_pretrained(repo_id="hugging-quants/Llama-3.2-3B-Instruct-Q8_0-GGUF", filename="*q8_0.gguf")
 
         system_prompt = "You are an artist, describe a beautiful scene using the words from the user."
+
+        sys_pat = "system:.*\n\n"
+        system = re.match(sys_pat, prompt, flags=re.M|re.I)
+        if system is not None:
+            system_prompt = re.sub("system: *", "", system.group(0), flags=re.M|re.I)
+            prompt = re.sub(sys_pat, "", prompt)
 
         res = llm.create_chat_completion(
             messages=[
