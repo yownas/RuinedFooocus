81395ef64a572504a92c1e6c853839e540d7424d
Jonas Stare <yownas@gmail.com>
2024-10-02 22:46:23 +0200

    Test with selectable Llama assistants

--- ui_llama.py
+++ ui_llama.py
@@ -2,23 +2,30 @@ import gradio as gr
 import re
 from llama_cpp import Llama
 from modules.util import TimeIt
+from pathlib import Path
 
 def add_llama_tab(prompt):
-    def run_llama_run(prompt):
+    def run_llama_run(system_file, prompt):
         repo = "hugging-quants/Llama-3.2-3B-Instruct-Q8_0-GGUF"
         print(f"Loading {repo}")
         llm = Llama.from_pretrained(repo_id=repo , filename="*q8_0.gguf", verbose=False)
 
-        system_prompt = "You are an artist, describe a beautiful scene using the words from the user."
 
         sys_pat = "system:.*\n\n"
         system = re.match(sys_pat, prompt, flags=re.M|re.I)
-        if system is not None:
+        if system is not None: # Llama system-prompt provided in the ui-prompt
+            name = "Llama"
             system_prompt = re.sub("^[^:]*: *", "", system.group(0), flags=re.M|re.I)
             prompt = re.sub(sys_pat, "", prompt)
+        else:
+            file = open(system_file, "r")
+            name = file.readline().strip()
+            system_prompt = file.read().strip()
 
-        print("Thinking...")
         with TimeIt(""):
+            print(f"# System:\n{system_prompt.strip()}\n")
+            print(f"# User:\n{prompt.strip()}\n")
+            print(f"# {name}: (Thinking...)")
             try:
                 res = llm.create_chat_completion(
                     messages=[
@@ -29,17 +36,34 @@ def add_llama_tab(prompt):
                 print(f"LLAMA ERROR: {e}")
                 res = prompt
 
-            print(f"# System:\n{system_prompt.strip()}\n")
-            print(f"# User:\n{prompt.strip()}\n")
-            print(f"# Assistant:\n{res.strip()}\n")
+            print(f"{res.strip()}\n")
 
         llm._stack.close()
         llm.close()
 
         return gr.update(value=res)
 
-    llama_btn = gr.Button(
-        value="🦙",
-        min_width=1,
-    )
-    llama_btn.click(run_llama_run, inputs=[prompt], outputs=[prompt])
+    def llama_names():
+        names = []
+        folder_path = Path("llamas")
+        for path in folder_path.rglob("*"):
+            if path.suffix.lower() in [".txt"]:
+                f = open(path, "r")
+                name = f.readline().strip()
+                names.append((name, path))
+
+        return names
+
+    with gr.Group(), gr.Row():
+        llama_select = gr.Dropdown(
+            choices=llama_names(),
+            show_label=False,
+            scale=4,
+        )
+        llama_btn = gr.Button(
+            value="🦙",
+            scale=1,
+            min_width=1,
+        )
+
+    llama_btn.click(run_llama_run, inputs=[llama_select, prompt], outputs=[prompt])
