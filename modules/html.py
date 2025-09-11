b659f6e459fa987611c66d778821646379c1a7e3
Jote soft <jhotaysoft@gmail.com>
2025-09-11 09:46:27 +0600

    Update html.py fix generating problem from from mobile
    i have modified the html.py and made some change to added css to fix error in UI. which prevented clicking/taping on generate button from mobile version. 
    
    - generation from phone is working now. no need to change to desktop mode.
    - larger prompt is now scrollable and easily editable from both pc and mobile.
    - mobile version now looks like actual mobile version.

--- modules/html.py
+++ modules/html.py
@@ -4,9 +4,19 @@ css = """
   align-items: center; /* Center items vertically within the container */
   white-space: nowrap; /* Prevent line breaks within the container */
   overflow-x:hidden;
-  overflow-y:hidden;
+  overflow-y:scroll;
 }
-
+/* custom css start*/
+#component-19 {
+    position: ;
+}
+#component-7 {
+    height: 100% !important;
+}
+.gradio-container.gradio-container-5-35-0 .contain .type_row{
+    height: 150px;
+}
+/* custom css ends*/
 /* Style the progress bar */
 progress {
   appearance: none; /* Remove default styling */
@@ -15,23 +25,19 @@ progress {
   background-color: #f3f3f3; /* Light grey background */
   width: 100%;
 }
-
 /* Style the progress bar container */
 .progress-container {
   margin-left: 20px;
   margin-right: 20px;
   flex-grow: 1; /* Allow the progress container to take up remaining space */
 }
-
 /* Set the color of the progress bar fill */
 progress::-webkit-progress-value {
   background-color: #3498db; /* Blue color for the fill */
 }
-
 progress::-moz-progress-bar {
   background-color: #3498db; /* Blue color for the fill in Firefox */
 }
-
 /* Style the text on the progress bar */
 progress::after {
   content: attr(value '%'); /* Display the progress value followed by '%' */
@@ -42,56 +48,65 @@ progress::after {
   color: white; /* Set text color */
   font-size: 14px; /* Set font size */
 }
-
 /* Style other texts */
 .loader-container > span {
   margin-left: 5px; /* Add spacing between the progress bar and the text */
 }
-
 .progress-bar > .generating {
   display: none !important;
 }
-
 .progress-bar{
   height: 30px !important;
 }
-
 .hint-container > .generating {
   display: none !important;
 }
-
 .hint-container{
   height: 150px !important;
 }
-
 .json-container{
   height: 600px;
   overflow: auto !important;
-  }
-
+}
 .type_row{
-  height: 96px !important;
+  height: 150px !important;
 }
 .type_small_row{
-  height: 40px !important;
+  height: 100px !important;
 }
-
 .scroll-hide{
-  resize: none !important;
+  resize: auto !important;
 }
-
 .refresh_button{
   border: none !important;
   background: none !important;
   font-size: none !important;
   box-shadow: none !important;
 }
-
 .element1 {
   opacity: 0.01;
 }
-
 #inpaint_sketch { overflow: overlay !important; resize: auto; background: var(--panel-background-fill); z-index: 5; }
+/* Custom CSS for mobile-friendliness */
+@media (max-width: 768px) {
+    body, .gr-textbox, .gr-button {
+        font-size: 14px;
+    }
+ #component-321
+ {
+    padding: unset;
+}  
+    
+#component-7 {
+height: 100% !important;
+}
+#component-19{
+    padding: 0px 6% 0 7%;
+}
+#component-22{
+    margin-top: 55px;
+}
+}
 """
 progress_html = """
 <div class="loader-container">
